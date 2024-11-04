b2176d909f3d4d7d1772ee60747db3063437862d
Michail Chatzis <mchatzis3@gmail.com>
2024-11-04 09:18:20 +0100

    Added bidirectional backpopulates for location-organisation

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from sqlmodel import SQLModel, Field, Relationship
-
+from typing import List
 
 class Base(SQLModel):
     pass
@@ -11,12 +11,18 @@ class CreateOrganisation(Base):
 class Organisation(Base, table=True):
     id: int | None = Field(primary_key=True)
     name: str
+    locations: List["Location"] = Relationship(back_populates="organisation")
 
+class CreateLocation(Base):
+    organisation_id: int
+    location_name: str
+    longitude: float
+    latitude: float
 
 class Location(Base, table=True):
     id: int | None = Field(primary_key=True)
     organisation_id: int = Field(foreign_key="organisation.id")
-    organisation: Organisation = Relationship()
+    organisation: Organisation = Relationship(back_populates="locations")
     location_name: str
     longitude: float
     latitude: float
